236c84850e02ca8a99e1fee5c84ad4cb5ce934b8
Kevin Andres Forero Guaitero <42241664+kevinandresforero@users.noreply.github.com>
2019-08-22 06:12:06 -0500

    Intento II

--- MaxSimplex.py
+++ MaxSimplex.py
@@ -3,8 +3,8 @@ import numpy
 print()
 print("     -----   Maximización Por Simplex    -----           ")
 print()
-NumVar = int(input("    Ingrese El Número De Variables  :   "))
-NumRest = int(input("    Ingrese El Número De Restriciones   :   "))
+NumVar = int(input("    Ingrese El Número De Variables    :   "))
+NumRest = int(input("    Ingrese El Número De Restriciones  :   "))
 print()
 '''
 
@@ -22,4 +22,19 @@ for c in range (0,NumRest):
         MatrizRstr[c,f] = input(" La Variable " + str(f+1) + " De La Restricción " + str(c+1) + " : ")
 print()
 
+print(" la matriz de restriccíon es :   ")
+print()
 print(MatrizRstr)
+print()
+for c in range (0,NumRest+1):
+    for f in range (0,NumVar):
+        if (c >= NumRest):
+            TablaSimplex[c,f] = input("Ingrese El valor De La Variable " + str(f+1) + " De La Ecuacion Objetivo : ")
+        else:
+            TablaSimplex[c,f] = MatrizRstr[c,f]
+            print(MatrizRstr[c,f])
+print()
+print(" la Tabla simplex es :   ")
+print()
+print(TablaSimplex)
+print()
