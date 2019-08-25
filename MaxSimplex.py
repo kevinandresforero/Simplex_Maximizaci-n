615f573e91841e54ca4d680944596132e88df52b
Kevin Andres Forero Guaitero <42241664+kevinandresforero@users.noreply.github.com>
2019-08-25 11:25:33 -0500

    Terminando matriz con la tabla simplex

--- MaxSimplex.py
+++ MaxSimplex.py
@@ -12,29 +12,57 @@ print()
             TablaSimplex = MatrizRstr + la ecuación objetivo
 
 '''
+
+NumCol = NumRest + NumVar +2
+NumFil = NumRest +1
+
 MatrizRstr = numpy.zeros((NumRest , NumVar))
-TablaSimplex = numpy.zeros((NumRest + 1 , NumVar + 1))
+TablaSimplex = numpy.zeros((NumRest + 1 , NumVar + NumRest + 2))
+Resultado = list(range(NumRest))
+Holgura = numpy.zeros((NumRest, NumRest))
 
 print(" Ingrese El Valor de: ")
 print()
-for c in range (0,NumRest):
+for c in range(0,NumRest):
     for f in range (0,NumVar):
         MatrizRstr[c,f] = input(" La Variable " + str(f+1) + " De La Restricción " + str(c+1) + " : ")
+
 print()
 
+
+for h in range(0,NumRest):
+    Resultado[h] = input("Ingrese el resultado de la restricción "+ str(h+1) +" : ")
+
+print()
 print(" la matriz de restriccíon es :   ")
 print()
 print(MatrizRstr)
 print()
-for c in range (0,NumRest+1):
-    for f in range (0,NumVar):
-        if (c >= NumRest):
-            TablaSimplex[c,f] = input("Ingrese El valor De La Variable " + str(f+1) + " De La Ecuacion Objetivo : ")
-        else:
-            TablaSimplex[c,f] = MatrizRstr[c,f]
-            print(MatrizRstr[c,f])
+
+'''Holgura'''
+for f in range(0, NumRest):
+    for c in range(0, NumRest):
+        if (f == c):
+            Holgura[f,c] = 1
+
+print(Holgura)
 print()
-print(" la Tabla simplex es :   ")
+
+for f in range(0, NumFil):
+    for c in range(0, NumCol):
+        if(c==0):
+            if(f < NumRest):
+                TablaSimplex[f,c] = 0
+            else:
+                TablaSimplex[f,c] = 1
+        else:
+            if (f == NumFil-1 and c < NumVar+1):
+                TablaSimplex[f,c] = -1*(float(input("Ingrese El valor De La Variable " + str(c) + " De La Ecuacion Objetivo : ")))
+        if(c>NumVar and c<NumCol-1 and f<NumFil-1):
+                    TablaSimplex[f,c] = Holgura[f,c-NumVar-1 ]
+
+print()
+print(" la Tabla simplex es ? :   ")
 print()
 print(TablaSimplex)
 print()
